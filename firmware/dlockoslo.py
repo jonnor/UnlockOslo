de995c31d1036099ff2d8931accbbd6bbbb8dca9
Jon Nordby <jononor@gmail.com>
2017-11-12 17:57:01 +0100

    firmware: Basic GPIO setup/input

--- firmware/dlockoslo.py
+++ firmware/dlockoslo.py
@@ -2,6 +2,10 @@
 import typing
 import numbers
 
+import gevent
+import os
+import time
+
 class Inputs(typing.NamedTuple):
     openbutton_outside: bool = False  # True if pressed
     openbutton_inside: bool  = False
@@ -81,4 +85,88 @@ def next_state(current: States, inputs: Inputs) -> States:
 
     return state 
 
+def gpio_file_path(number: int):
+    return '/sys/class/gpio/gpio{}/value'.format(number)
 
+# idempotent
+def setup_gpio_pin(number : int, direction):
+    export_path = '/sys/class/gpio/export'
+    direction_path = '/sys/class/gpio{}/direction'.format(number)
+
+    # Export GPIO
+    if not path.exists(direction_path):
+        with open(export_path, 'r') as exportfile:
+            exportfile.write(str(number))
+        # wait for export done, otherwise direction change fails EACCESS
+        time.sleep(10)
+    
+    # Set direction
+    with open(direction_path, 'w') as directionfile:
+        directionfile.write(direction)
+
+    # TODO: configure drive current?
+
+def read_boolean(file_path):
+    with open(file_path) as f:
+        s = f.read()
+        return s == '1'
+
+def get_inputs(input_files) -> Inputs:
+    gpio_inputs = { i: read_boolean(p) for i, p in input_files.items() }
+
+    b = dict(
+        current_time=time.monotonic()
+    )
+    i = dict()
+    i.update(gpio_inputs)
+    i.update(b) 
+
+    return Inputs(i)
+
+
+def setup_gpio(pinmap):
+    input_files = {}
+    output_files = {}
+
+    for name, pinconfig in pinmap.items():
+        print(pinconfig)
+
+        direction, gpio = pinconfig
+        if isinstance(gpio, int):
+            setup_gpio_pin(gpio, direction)
+            path = gpio_file_path(gpio)
+        else:
+            # assumed to be a regular file that can be read/written to
+            path = gpio
+
+        files = input_files if direction == 'input' else output_files
+        files[path] = path
+
+    return input_files, output_files
+
+def main():
+    pin_mapping = {
+        # in
+        'holdopen_button': ('input', 10),
+        'openbutton_outside': ('input', 11),
+        'openbutton_inside': ('input', 12),
+        # out
+        'lock': ('output', 13),
+        'opener': ('output', 14),
+        'connected_light': ('output', 15),
+    }
+
+    # TEMP: testing using plain-files
+    pin_mapping = { p: (cfg[0], 'gpio{}'.format(cfg[1])) for p, cfg in pin_mapping.items() } 
+
+    input_files, output_files = setup_gpio(pin_mapping)
+
+    state = States()
+    while True:
+        i = get_inputs(input_files)
+        state = next_state(state, i)        
+        print(i, state)
+        time.sleep(0.2)
+
+if __name__ == '__main__':
+    main()
