9f103f3fffd1df5508c3c36717ae1a27f0913518
Jon Nordby <jononor@gmail.com>
2017-11-13 22:56:45 +0100

    gateway: Basic

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -13,12 +13,27 @@ import flask
 
 import os
 
+import paho.mqtt.client as mqtt
+
+def send_mqtt():
+    # TODO: keep client connected
+    client = mqtt.Client()
+    #client.username_pw_set(broker_info.username, broker_info.password)
+
+    host = 'localhost'
+    port = 1883
+    client.connect(host, port, 60)
+
+    # FIXME: unhardcode
+    topic = "firmware/dlockoslo.py.UNLOCK"
+    payload = '10'
+    client.publish(topic, payload)
 
 app = flask.Flask(__name__)
 ## System functionality
 @app.route('/')
 def index():
-  return 'dlock-oslo gateway'
+  return 'dlock-oslo HTTP gateway'
 
 @app.route('/status')
 def system_status():
@@ -28,14 +43,19 @@ def system_status():
 ## Door functionality
 @app.route('/doors/<doorid>/unlock', methods=['POST'])
 def door_unlock(doorid):
-    return 'Hello World'
+    # FIXME: wait for and verify state change message
+    send_mqtt()
+    return 'Door is now open'
 
 @app.route('/doors/<doorid>/lock', methods=['POST'])
 def door_lock(doorid):
-    return 'Hello World'
+    # FIXME: send lock message
+    # FIXME: wait for and verify state change message
+    return 'Door is now locked'
 
 @app.route('/doors/<doorid>/state')
 def door_state(doorid):
+    # TODO: return current state of door, as reported on MQTT
     raise NotImplementedError("Unknown system status")
 
 
