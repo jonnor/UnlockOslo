a59d341d6064ff7496bf066cb298ddbc697ad479
Jon Nordby <jononor@gmail.com>
2017-11-21 13:51:04 +0100

    gateway: Fix running of multiple test devices
    References msgflo/msgflo-python#36

--- gateway/testdevices.py
+++ gateway/testdevices.py
@@ -3,24 +3,35 @@ import os
 import os.path
 import sys
 
-sys.path.insert(0, './firmware')
+firmware_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../firmware')
+sys.path.insert(0, firmware_path)
 import dlockoslo
 import msgflo
+import gevent
 
 def create_virtual_lock(name):
     os.environ['DLOCK_FAKE_GPIO'] = name
     if not os.path.exists(name):
         os.mkdir(name)
-    virtual = msgflo.main(dlockoslo.LockParticipant, role=name)
+    virtual = dlockoslo.LockParticipant(role=name)
     del os.environ['DLOCK_FAKE_GPIO']
     return virtual
 
+
+def get_participants():
+    participants = [
+        create_virtual_lock('virtual-1'),
+        create_virtual_lock('virtual-2'),
+        dlockoslo.AlwaysErroringParticipant(role='erroring-1'),
+    ]
+    # for emulating timeout/device missing, send on MQTT topic which nothing uses
+    return participants
+
 def main():
-    virtual1 = create_virtual_lock('virtual-1')
-    virtual2 = create_virtual_lock('virtual-2')
-    erroring = msgflo.main(dlockoslo.AlwaysErroringParticipant, role='erroring-1')
-    # for emulating timeout/device missing, send on MQTT topic which nothing
+    participants = get_participants()
+    waiter = gevent.event.AsyncResult()
+    engine = msgflo.run(participants, done_cb=waiter.set)
+    waiter.wait()
 
 if __name__ == '__main__':
-    print(__file__)
     main()
