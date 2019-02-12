da61b4f99bd87a7d3ad1810b90e9606200361ab6
Jon Nordby <jononor@gmail.com>
2019-02-12 12:24:08 +0100

    firmware: Tests for door presence

--- firmware/test_doorsystem.py
+++ firmware/test_doorsystem.py
@@ -166,3 +166,64 @@ def test_dooropener_after_unlock():
     states = dlockoslo.next_state(states, dlockoslo.Inputs(**inputs))
     assert states.opener.state == 'TemporarilyActive'
 
+
+def test_doorpresent_reflects_input():
+    states = dlockoslo.States()
+    assert states.lock.state == 'Locked'
+    assert states.opener.state == 'Inactive'
+    inputs = dict(
+        door_present=False,
+    )
+    states = dlockoslo.next_state(states, dlockoslo.Inputs(**inputs))
+    assert states.lock.state == 'Locked'
+    assert not states.door_present
+
+    # False -> True
+    inputs = dict(
+        door_present=True,
+    )
+    states = dlockoslo.next_state(states, dlockoslo.Inputs(**inputs))
+    assert states.lock.state == 'Locked'
+    assert states.door_present
+
+    # True -> False
+    inputs = dict(
+        door_present=False,
+    )
+    states = dlockoslo.next_state(states, dlockoslo.Inputs(**inputs))
+    assert states.lock.state == 'Locked'
+    assert not states.door_present
+
+
+@pytest.mark.parametrize('door_present', [True, False])
+def test_doorpresent_periodic_update(door_present):
+
+    states = dlockoslo.States()
+    inputs = dict(
+        door_present=door_present,
+    )
+    states = dlockoslo.next_state(states, dlockoslo.Inputs(**inputs))
+    assert states.lock.state == 'Locked'
+    assert states.door_present == door_present
+    last_updated = states.door_present_updated
+
+    inputs = dict(
+        door_present=door_present,
+        current_time=30.0, # shorter than update setting
+    )
+    states = dlockoslo.next_state(states, dlockoslo.Inputs(**inputs))
+    assert states.lock.state == 'Locked'
+    assert states.door_present == door_present
+    assert states.door_present_updated == last_updated, 'should not update'
+
+    inputs = dict(
+        door_present=door_present,
+        current_time=90.0, # longer than update setting
+    )
+    states = dlockoslo.next_state(states, dlockoslo.Inputs(**inputs))
+    assert states.lock.state == 'Locked'
+    assert states.door_present == door_present
+    assert states.door_present_updated > last_updated, 'should update'
+    assert states.door_present_updated == 90.0
+
+
