5f35259df5075930548df028fa2eebe4ddf6cb2e
Torfinn Ingolfsen <tingox@gmail.com>
2020-06-04 17:06:47 +0200

    dlock-35 is now rachel-grepp-1

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -271,7 +271,7 @@ doors = {
     'deichman-torshov': DoorInfo('doors/dlock-32'),
     'unused-33': DoorInfo('doors/dlock-33'),
     'unused-34': DoorInfo('doors/dlock-34'),
-    'unused-35': DoorInfo('doors/dlock-35'),
+    'rachel-grepp-1': DoorInfo('doors/dlock-35'),
     'unused-36': DoorInfo('doors/dlock-36'),
     'unused-37': DoorInfo('doors/dlock-37'),
     'unused-38': DoorInfo('doors/dlock-38'),
